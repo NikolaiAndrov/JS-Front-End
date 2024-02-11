f462faadb54aa2becef840baab9671f6a321d2e9
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-11 23:00:26 +0200

    pascal case splitter

---
+++ JS-Arrays-And-Strings–Exercises/pascalCaseSplitter.js
@@ -0,0 +1,7 @@
+function spliter(text) {
+    let regex = /[A-Z][a-z]*/gm;
+    let matches = text.match(regex);
+    console.log(matches.join(", "));
+}
+
+spliter('SplitMeIfYouCanHaHaYouCantOrYouCan');
