36d4c773fa194bfb3ae24f116b523958960af6eb
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-11 21:32:43 +0200

    sort numbers done

--- JS-Arrays-And-Strings–Exercises/sortingNumbers.js
+++ JS-Arrays-And-Strings–Exercises/sortingNumbers.js
@@ -1,3 +1,17 @@
 function sortNumbers(array) {
-    array.sort((a, b) => a.localeCompare(b));
+
+    array.sort((a, b) => a - b);
+    let iterations = parseInt(array.length / 2);
+    let result = [];
+
+    for (let index = 0; index < iterations; index++) {
+        result.push(array[index]);
+        result.push(array[array.length - 1 - index]);
+    }
+
+    if (array.length % 2 !== 0) {
+        result.push(array[iterations]);
+    }
+
+    return result;
 }
