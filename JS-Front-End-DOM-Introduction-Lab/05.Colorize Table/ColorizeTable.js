72111299119120173545797a265b2dc007d5a360
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-26 22:44:30 +0200

    Colorize Table

--- JS-Front-End-DOM-Introduction-Lab/05.Colorize Table/ColorizeTable.js
+++ JS-Front-End-DOM-Introduction-Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
-}
+    const rows = document.getElementsByTagName("tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        
+        if (i % 2 !== 0) {
+            rows[i].style.background = "teal";
+        }
+    }
+}   
