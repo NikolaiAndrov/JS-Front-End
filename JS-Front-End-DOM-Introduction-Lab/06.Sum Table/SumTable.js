6bff9270e5799e009c75c95b8e1af9de7501347f
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-26 23:22:11 +0200

    Sum Table

--- JS-Front-End-DOM-Introduction-Lab/06.Sum Table/SumTable.js
+++ JS-Front-End-DOM-Introduction-Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+    const elements = document.getElementsByTagName("tr");
+    let sum = 0;
+    for (let i = 1; i < elements.length - 1; i++) {
+        
+        const cells = elements[i].children;
+        const currentPrice = Number(cells[1].textContent);
+        sum += currentPrice;
+    }
 
+    const result = document.getElementById("sum");
+    result.textContent = sum
 }
