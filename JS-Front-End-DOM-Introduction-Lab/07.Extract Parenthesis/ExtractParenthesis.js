d218b33f41b23636e4ae17325fb0cb773ce41de5
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-26 23:55:34 +0200

    Extract Parenthesis

--- JS-Front-End-DOM-Introduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS-Front-End-DOM-Introduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,7 @@
 function extract(content) {
-
+    let text = document.getElementById(content).textContent;
+    let matches = text.match(/\(([^)]+)\)/g);
+    const extractedText = matches.map(match => match.slice(1, -1));
+    
+    return extractedText.join("; ");
 }
