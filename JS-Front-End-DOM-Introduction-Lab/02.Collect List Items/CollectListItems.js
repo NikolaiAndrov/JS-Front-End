0dd34f54ac19b2885b6a06ae2dd731ac62debef9
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-26 21:37:31 +0200

    Collect List

--- JS-Front-End-DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
+++ JS-Front-End-DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+
+    const elements = document.getElementsByTagName("li");
+    const textElements = [];
+
+    for (const element of Array.from(elements)) {
+        textElements.push(element.textContent);
+    }
+
+    const result = document.getElementById("result"); 
+    result.textContent = textElements.join("\n");
 }
