34abf922e4db6c547521f52f1b1aba212da4745e
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-27 16:37:04 +0200

    Pascal or Camel Case

--- JS-Front-End-DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ JS-Front-End-DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+
+  let text = document.querySelector("#text").value.toLocaleLowerCase().split(" ");
+  const convention = document.querySelector("#naming-convention").value;
+  let words = [];
+
+  if (convention === "Camel Case") {
+
+    words.push(text[0]);
+
+    for (let i = 1; i < text.length; i++) {
+      const element = text[i];
+
+      const char = element.charAt(0).toUpperCase();
+      const wordPart = element.slice(1);
+      const entireWord = char + wordPart;
+      words.push(entireWord)
+    }
+    
+  } else if (convention === "Pascal Case") {
+
+    for (let i = 0; i < text.length; i++) {
+      const element = text[i];
+
+      const char = element.charAt(0).toUpperCase();
+      const wordPart = element.slice(1);
+      const entireWord = char + wordPart;
+      words.push(entireWord)
+    }
+
+  } else {
+    words.push("Error!");
+  }
+
+  document.querySelector("#result").textContent = words.join("");
 }
