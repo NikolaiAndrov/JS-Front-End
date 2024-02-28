260c69c59a81bb6865ae6201d31cb13eaeb951d4
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-28 14:35:09 +0200

    Table – Search Engine

--- JS-Front-End-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ JS-Front-End-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   let keyword = document.getElementById("searchField");
+   let tableElements = document.querySelectorAll("tbody tr");
+   const className = "select";
 
+   function onClick() {
+
+      for (const row of tableElements) {
+
+         row.classList.remove(className);
+
+         if (row.textContent.includes(keyword.value)) {
+            row.className = className;
+         }
+      }
+
+      keyword.value = "";
    }
 }
