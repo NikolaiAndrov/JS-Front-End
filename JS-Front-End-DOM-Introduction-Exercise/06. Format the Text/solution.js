7ad2b3683e5053f94265ebebf6963d78b2a2d650
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-28 16:05:29 +0200

    Format the Text

--- JS-Front-End-DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ JS-Front-End-DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,9 @@
 function solve() {
-  //TODO
+  let text = document.getElementById("input").value.split(".").filter(t => t.length > 0).map(t => t += ".");
+  
+  while (text.length > 0) {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text.splice(0, 3).join("");
+    document.getElementById("output").appendChild(paragraph);
+  }
 }
