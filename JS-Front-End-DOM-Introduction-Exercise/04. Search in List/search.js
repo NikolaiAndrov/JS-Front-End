39a5a95404f41f264b2fabaf69f0c86d9b8c3324
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-28 14:02:20 +0200

    Search in List

--- JS-Front-End-DOM-Introduction-Exercise/04. Search in List/search.js
+++ JS-Front-End-DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+
+   let listItems = document.querySelectorAll("li");
+   let keyword = document.querySelector("#searchText").value;
+   let matchesCount = 0;
+
+   for (const li of listItems) {
+      li.style.fontWeight = "";
+      li.style.textDecoration = "";
+   }
+
+   for (const li of listItems) {
+      if (li.textContent.includes(keyword)) {
+         matchesCount++;
+         li.style.fontWeight = "bold";
+         li.style.textDecoration = "underline";
+      }
+   }
+
+   document.querySelector("#result").textContent = `${matchesCount} matches found`;
 }
