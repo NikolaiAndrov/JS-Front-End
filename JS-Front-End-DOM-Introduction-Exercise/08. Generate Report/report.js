231e0beea1f8f33f0b78112ef9b1753106007699
nikolaiandrov <nikolaiandrov@abv.bg>
2024-02-29 13:05:01 +0200

    Generate Report

--- JS-Front-End-DOM-Introduction-Exercise/08. Generate Report/report.js
+++ JS-Front-End-DOM-Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,31 @@
 function generateReport() {
-    //TODO
+    let personObject = {};
+    let allPeople = [];
+    let columnSetNumber = [];
+
+    let columnsAll = document.querySelectorAll("thead tr th input");
+   
+    for (let i = 0; i < columnsAll.length; i++) {
+        
+        if (columnsAll[i].checked) {
+            columnSetNumber.push(i);
+        }
+    }
+
+    let columnTitles = document.querySelectorAll("thead tr")[0].getElementsByTagName("th");
+    let rowsCount = document.querySelectorAll("tbody tr").length;
+
+    for (let row = 0; row < rowsCount; row++) {
+
+        for (const element of columnSetNumber) {
+            let key = columnTitles[element].textContent.trim().toLowerCase();
+            let value = document.querySelectorAll("tbody tr")[row].getElementsByTagName("td")[element].textContent;
+            personObject[key] = value;
+        }
+
+        allPeople.push(Object.assign(personObject));
+        personObject = {};
+    }
+
+    document.getElementById("output").innerHTML = JSON.stringify(allPeople);
 }
