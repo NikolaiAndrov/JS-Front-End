77d9fa39dd75e5f74411177fb212ce2ecf896fd6
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-04 22:30:56 +0200

    Delete from Table

--- JS-DOM-Manipulation-And-Events/02. Delete-From-Table/app.js
+++ JS-DOM-Manipulation-And-Events/02. Delete-From-Table/app.js
@@ -1,3 +1,23 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    let emailToDelete = document.querySelector("input[name = 'email']");
+    let allEmails = document.querySelectorAll("tbody tr");
+    let isDeleted = false;
+    
+    for (const emailRow of allEmails) {
+
+        if (emailRow.children[1].textContent === emailToDelete.value) {
+            emailRow.remove();
+            isDeleted = true;
+        }
+    }
+
+    emailToDelete.value = "";
+    let result = document.querySelector("#result");
+
+    if (isDeleted) {
+        result.textContent = "Deleted.";
+    } else {
+        result.textContent = "Not found.";
+    }
 }
