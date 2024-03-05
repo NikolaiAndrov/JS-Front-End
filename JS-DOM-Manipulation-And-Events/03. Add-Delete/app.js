c42d43b87b0c3b018e94f9e1961d5d6e0e557914
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-05 12:42:25 +0200

    Add / Delete

--- JS-DOM-Manipulation-And-Events/03. Add-Delete/app.js
+++ JS-DOM-Manipulation-And-Events/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    let newInputItem = document.querySelector("#newItemText");
+    let newListItem = document.createElement("li");
+
+    newListItem.textContent = newInputItem.value;
+    document.querySelector("#items").appendChild(newListItem);
+    newInputItem.value = "";
+
+    let deleteLink = document.createElement("a");
+    deleteLink.textContent = "[Delete]";
+    deleteLink.href = "#";
+    newListItem.appendChild(deleteLink);
+
+    deleteLink.addEventListener("click", deleteElement);
+
+    function deleteElement(e) {
+        let rowToDelete = e.currentTarget.parentNode;
+        rowToDelete.remove();
+    }
 }
