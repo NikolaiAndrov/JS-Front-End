77ff05d05a7a382c4a3b34aeece358d824cd74d5
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-05 14:21:44 +0200

    Dynamic Validation

--- JS-DOM-Manipulation-And-Events/06. Dynamic-Validation/app.js
+++ JS-DOM-Manipulation-And-Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+
+    let input = document.querySelector("#email");
+    input.addEventListener("change", onChange);
+
+    function onChange(e) {
+
+        let regex = /^[a-z]+@[a-z]+\.[a-z]+$/gm;
+
+        if (!regex.test(input.value)) {
+
+            input.classList.add("error");
+        } else {
+
+            input.classList.remove("error");
+        }
+    }
 }
