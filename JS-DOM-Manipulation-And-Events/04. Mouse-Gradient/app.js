285f15ef84f5ce67f811556c33b3881b1d298bf5
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-05 12:57:53 +0200

    Mouse Gradient

--- JS-DOM-Manipulation-And-Events/04. Mouse-Gradient/app.js
+++ JS-DOM-Manipulation-And-Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.querySelector("#gradient");
+    let result = document.querySelector("#result");
+
+    gradient.addEventListener("mousemove", move);
+    gradient.addEventListener("mouseout", out);
+
+    function move(e) {
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        result.textContent = power + "%";
+    }
+
+    function out(e) {
+        result.textContent = "";
+    }
 }
