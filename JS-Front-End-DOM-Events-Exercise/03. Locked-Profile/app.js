1a1d1667260150c707d83d2a479bd1eebbf3cd5f
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-07 15:35:44 +0200

    Locked Profile

--- JS-Front-End-DOM-Events-Exercise/03. Locked-Profile/app.js
+++ JS-Front-End-DOM-Events-Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,36 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let buttons = document.getElementsByTagName("button");
+   
+    for (const button of buttons) {
+        button.addEventListener("click", showInfo);
+    }
+
+    function showInfo(event) {
+
+        let button = event.currentTarget;
+        
+        let profile = button.parentElement;
+        let children = profile.children;
+
+        let isLocked = children[2].checked;
+        
+        if (isLocked) {
+            return;
+        }
+
+        let additionalInfo = children[9];
+        
+        if (button.textContent === "Show more") {
+
+            additionalInfo.style.display = "block";
+            button.textContent = "Hide it";
+
+        } else if (button.textContent === "Hide it") {
+
+            additionalInfo.style.display = "none";
+            button.textContent = "Show more";
+
+        }
+    }
 }
