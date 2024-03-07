f28362f11738c6cdeb327d87a0b42c63cd63f712
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-07 13:19:56 +0200

    Sections

--- JS-Front-End-DOM-Events-Exercise/01. Sections/script.js
+++ JS-Front-End-DOM-Events-Exercise/01. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+   
+   let content = document.getElementById("content");
+   
+   for (const word of words) {
+
+      let div = document.createElement("div");
+      let paragraph = document.createElement("p");
+      
+      paragraph.textContent = word;
+      div.appendChild(paragraph);
+      
+      paragraph.style.display = "none";
+      div.addEventListener("click", displayContent);
+
+      content.appendChild(div);
+   }
+
+   function displayContent(event) {
+      let currentContent = event.currentTarget.querySelector("p");
+      currentContent.style.display = "";
+   }
 }
