06c48309c451f1e23406c143d207774e853a4608
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-07 16:33:47 +0200

    Message encoder and decoder

--- JS-Front-End-DOM-Events-Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS-Front-End-DOM-Events-Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,37 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let buttons = document.getElementsByTagName("button");
+    let textarea = document.getElementsByTagName("textarea");
+
+    let encodeButton = buttons[0];
+    encodeButton.addEventListener("click", encode)
+
+    let decodeButton = buttons[1]
+    decodeButton.addEventListener("click", decode)
+
+    function encode() {
+
+        let text = textarea[0].value;
+        let resultText = "";
+        
+        for (const char of text) {
+            resultText += String.fromCharCode(char.charCodeAt(0) + 1);
+        }
+
+       textarea[1].value = resultText;
+       textarea[0].value = "";
+    }
+
+    function decode() {
+
+        let text = textarea[1].value;
+        let resultText = "";
+
+        for (const char of text) {
+            resultText += String.fromCharCode(char.charCodeAt(0) - 1);
+        }
+
+        textarea[1].value = resultText;
+        decodeButton.disabled = true;
+    }
 }
