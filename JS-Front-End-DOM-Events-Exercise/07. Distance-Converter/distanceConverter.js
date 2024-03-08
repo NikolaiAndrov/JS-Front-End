bc4872721da4aa5a233faa107f460e7d4f9c9526
nikolaiandrov <nikolaiandrov@abv.bg>
2024-03-08 23:13:54 +0200

    Distance Converter

--- JS-Front-End-DOM-Events-Exercise/07. Distance-Converter/distanceConverter.js
+++ JS-Front-End-DOM-Events-Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,29 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    
+    let convertButton = document.getElementById("convert");
+    convertButton.addEventListener("click", convert);
+
+    function convert() {
+        
+        let distances = {
+            'km': 1000,
+            'm': 1,
+            'cm': 0.01,
+            'mm': 0.001,
+            'mi': 1609.34,
+            'yrd': 0.9144,
+            'ft': 0.3048,
+            'in': 0.0254
+        }
+
+        let inputDistance = document.getElementById("inputDistance").value;
+
+        let from = document.querySelector("#inputUnits").value;
+        let to = document.querySelector("#outputUnits").value;
+        debugger;
+        let result = inputDistance * distances[from] / distances[to];
+        
+        let outputDistance = document.getElementById("outputDistance");
+        outputDistance.value = result;
+    }
 }
